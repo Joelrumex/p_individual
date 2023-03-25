8eeb151e7ffdc0db5119aa9c3e3f43a190c2ee75
Joel <60098544+Joelrumex@users.noreply.github.com>
2023-03-25 12:12:49 +0100

    Numero de cartes i dificultat

--- pi_03/js/game_controller.js
+++ pi_03/js/game_controller.js
@@ -2,28 +2,40 @@ const back = "../resources/back.png";
 const items = ["../resources/cb.png","../resources/co.png","../resources/sb.png",
 "../resources/so.png","../resources/tb.png","../resources/to.png"];
 
-var json = localStorage.getItem("config") || '{"cards":2,"dificulty":"hard"}';
-
+var json = localStorage.getItem("config") || '{"cards":2,"dificultat":"hard"}';
+var potComençar = false;
+var temps = 0;
 var game = new Vue({
 	el: "#game_id",
 	data: {
 		username:'',
 		current_card: [],
 		items: [],
-		num_cards: 2,
+		nCartes: 2,
 		bad_clicks: 0
 	},
 	created: function(){
-		this.num_cards = JSON.parse(json).cards;
+		this.nCartes = JSON.parse(json).cards;
+		this.dificultat = JSON.parse(json).dificultat;
 		this.username = sessionStorage.getItem("username","unknown");
 		this.items = items.slice(); // Copiem l'array
 		this.items.sort(function(){return Math.random() - 0.5}); // Array aleatòria
-		this.items = this.items.slice(0, this.num_cards); // Agafem els primers numCards elements
+		this.items = this.items.slice(0, this.nCartes); // Agafem els primers numCards elements
 		this.items = this.items.concat(this.items); // Dupliquem els elements
 		this.items.sort(function(){return Math.random() - 0.5}); // Array aleatòria
 		for (var i = 0; i < this.items.length; i++){
 			this.current_card.push({done: false, texture: this.items[i]});
 		}
+		if (this.dificultat === "normal" ) temps = 500;
+		else if (this.dificultat === "easy" ) temps = 1000;
+		else if (this.dificultat === "hard" ) temps = 250;
+
+		setTimeout(() => {
+			for(var i = 0; i < this.items.length; i++){
+				Vue.set(this.current_card, i, {done: false, texture: back});
+			}
+			potComençar = true;
+		}, temps);
 	},
 	methods: {
 		clickCard: function(i){
@@ -31,9 +43,11 @@ var game = new Vue({
 				Vue.set(this.current_card, i, {done: false, texture: this.items[i]});
 		}
 	},
+
 	watch: {
 		current_card: function(value){
-			if (value.texture === back) return;
+			
+			if (value.texture === back || !potComençar) return;
 			var front = null;
 			var i_front = -1;
 			for (var i = 0; i < this.current_card.length; i++){
@@ -41,7 +55,7 @@ var game = new Vue({
 					if (front){
 						if (front.texture === this.current_card[i].texture){
 							front.done = this.current_card[i].done = true;
-							this.num_cards--;
+							this.nCartes--;
 						}
 						else{
 							Vue.set(this.current_card, i, {done: false, texture: back});
@@ -64,8 +78,3 @@ var game = new Vue({
 		}
 	}
 });
-
-
-
-
-
