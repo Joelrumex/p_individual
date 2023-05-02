0587efaa716b87f2a9702d3f8c699a4df49a33d7
Joel <60098544+Joelrumex@users.noreply.github.com>
2023-05-02 19:17:54 +0200

    Començar joc amb phaser
    estr

---
+++ treball_individual/js/game_controller1.js
@@ -0,0 +1,89 @@
+var json = localStorage.getItem("config") || '{"cards":2,"dificulty":"hard"}';
+
+
+class Example extends Phaser.Scene
+{
+    constructor ()
+    {
+        super();
+        this.cards = null;
+        this.numCards = JSON.parse(json).cards;
+		this.firstClick = null;
+		this.score = 100;
+		this.correct = 0;
+    }
+
+    preload ()
+    {
+        this.load.image('back', '../resources/back.png');
+		this.load.image('cb', '../resources/cb.png');
+		this.load.image('co', '../resources/co.png');
+		this.load.image('sb', '../resources/sb.png');
+		this.load.image('so', '../resources/so.png');
+		this.load.image('tb', '../resources/tb.png');
+		this.load.image('to', '../resources/to.png');
+    }
+
+    create (){	
+        let arraycards = ['cb', 'co', 'sb', 'so', 'tb', 'to'];
+		this.cameras.main.setBackgroundColor(0xBFFCFF);
+        arraycards = arraycards.slice(0, this.numCards);
+		arraycards = arraycards.concat(arraycards);
+		Phaser.Utils.Array.Shuffle(arraycards);
+        for (let index = 0; index < arraycards.length; index++) {
+            this.add.image(250+index*100, 300, arraycards[index]);
+        }
+        this.cards = this.physics.add.staticGroup();
+        for (let index = 0; index < arraycards.length; index++) {
+            this.cards.create(250+index*100, 300, 'back');
+        }
+		let i = 0;
+		this.cards.children.iterate((card)=>{
+			card.card_id = arraycards[i];
+			i++;
+			card.setInteractive();
+			card.on('pointerup', () => {
+				card.disableBody(true,true);
+				if (this.firstClick){
+					if (this.firstClick.card_id !== card.card_id){
+						this.score -= 20;
+						this.firstClick.enableBody(false, 0, 0, true, true);
+						card.enableBody(false, 0, 0, true, true);
+						if (this.score <= 0){
+							alert("Game Over");
+							loadpage("../");
+						}
+					}
+					else{
+						this.correct++;
+						if (this.correct >= 2){
+							alert("You Win with " + this.score + " points.");
+							loadpage("../");
+						}
+					}
+					this.firstClick = null;
+				}
+				else{
+					this.firstClick = card;
+				}
+			}, card);
+		});
+	}
+}
+
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1000,
+    height: 600,
+    parent: 'game_area',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: Example
+};
+
+const game = new Phaser.Game(config);
--- treball_individual/js/menu_controller.js
+++ treball_individual/js/menu_controller.js
@@ -1,23 +1,43 @@
+function joc() {
+	const mode = window.prompt('Selecciona el mode de joc (1 o 2)');
+    if (mode === '1') {
+		loadpage("./html/joc.html");
+    } else if (mode === '2') {
+		loadpage("./html/joc2.html");
+    } else {
+      window.alert('Mode de joc no vàlid');
+    }
+}
+
+function puntuacions() {
+	loadpage("./html/puntuacions.html");
+}
+
+function opcions() {
+	loadpage("./html/opcions.html");
+}
+
+function carregar() {
+	loadpage("./html/carregar.html");
+}
+
+function sortir() {
+	loadpage("../index.html");
+}
+
 var menu_controller = new Vue({
 	el: "#menu",
 	data: {
 		  menuTitle: "Memory menu",
+		  menuItems: [
+			{ label: "Jugar", action: () => joc() },
+			{ label: "Puntuacions", action: () => puntuacions() },
+			{ label: "Opcions", action: () => opcions() },
+			{ label: "Carregar", action: () => carregar() },
+			{ label: "Sortir", action: () => sortir() }
+		  ],
 	},
-	methods: {
-	joc: function() {
-		console.log("HOLA")
-	},
-	puntuacions: function() {
-		loadpage("./html/joc.html")
-	},
-	opcions: function() {
-		loadpage("./html/joc.html")
-	},
-	carregar: function() {
-		loadpage("./html/joc.html")
-	},
-	sortir: function() {
-		loadpage("./html/joc.html")
-	},
+	watch: {
+		
 	}
 });
